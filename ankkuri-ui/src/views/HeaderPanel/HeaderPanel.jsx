2f3be4983e79f392808fda3c472d050456e804e2
Bianca Jemsten <bianca.jemsten@sapient.com>
2019-11-17 01:26:41 +0200

    Fixed header link

--- ankkuri-ui/src/views/HeaderPanel/HeaderPanel.jsx
+++ ankkuri-ui/src/views/HeaderPanel/HeaderPanel.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { HeaderWrapper } from "./HeaderPanel.style";
+import { NavLink } from "react-router-dom";
 
 import { MainNav } from "../../components";
 
@@ -8,7 +9,9 @@ import Logo from "../../assets/logo-small.png";
 
 export default () => (
   <HeaderWrapper>
-    <img className="logo" src={Logo} alt="ankkuri" />
+    <NavLink exact to={`/`} className="menuItem-link" activeClassName="active">
+      <img className="logo" src={Logo} alt="ankkuri" />
+    </NavLink>
     <MainNav />
   </HeaderWrapper>
 );
