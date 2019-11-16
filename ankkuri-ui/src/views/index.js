2416b474f825a234b9ff4a4fd9184019ceceb2cd
Cat Oxley <catherine.oxley@live.co.uk>
2019-11-16 11:45:26 +0200

    Rename SlideDeck folder

--- ankkuri-ui/src/views/index.js
+++ ankkuri-ui/src/views/index.js
@@ -1,4 +1,4 @@
 export { default as HomePanel } from "./HomePanel/HomePanel";
 export { default as AboutPanel } from "./AboutPanel/AboutPanel";
-export { default as SlideDeck } from "./SlideDeckz/SlideDeck";
+export { default as SlideDeck } from "./SlideDeck/SlideDeck";
 export { default as HeaderPanel } from "./HeaderPanel/HeaderPanel";
