a89a95fbede03b84ac5cd612add60e0e0b1c3ffb
Bianca Jemsten <bianca.jemsten@sapient.com>
2019-11-17 08:06:51 +0200

    Made userpage dynamic

--- ankkuri-ui/src/views/ProfilePanel/ProfilePanel.jsx
+++ ankkuri-ui/src/views/ProfilePanel/ProfilePanel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // Components
 import Profile from "../../assets/avatar.jpeg";
@@ -11,27 +11,57 @@ import Stretching from "../../assets/icons/stretching.svg";
 import Scrolling from "../../assets/icons/scrolling.svg";
 import Breakfast from "../../assets/icons/breakfast.svg";
 import Shower from "../../assets/icons/shower.svg";
+import Beverage from "../../assets/icons/beverage.svg";
 import { GoalCard } from "../../elements/atoms";
+import { getUser } from "../../helpers/apiFunctions";
 
-export default () => (
-  <Wrapper>
-    <ProfileContainer>
-      <img src={Profile} alt="" className="avatar" />
-      <h1 className="h3">Catherine Oxley</h1>
-      <p className="streak">5 day streak 🔥</p>
+export default () => {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedUser = await getUser();
+      console.log(fetchedUser[0]);
 
-      <h5>My Favourites</h5>
-      <CardsContainer>
-        <ActivityCard icon={Stretching} title="Stretching" time="5 mins" />
-        <ActivityCard icon={Scrolling} title="No Scrolling" />
-        <ActivityCard icon={Breakfast} title="Breakfast" />
-        <ActivityCard icon={Shower} title="Shower" time="15 mins" />
-      </CardsContainer>
+      setUser(fetchedUser[0]);
+    }
+    fetchData();
+  }, []);
+  return user ? (
+    <Wrapper>
+      <ProfileContainer>
+        <img src={Profile} alt="" className="avatar" />
+        <h1 className="h3">{user.name}</h1>
+        <p className="streak">{user.streak} day streak 🔥</p>
 
-      <GoalContainer>
-        <h5>Goal Progress</h5>
-        <GoalCard progress={5} maxDays={7} />
-      </GoalContainer>
-    </ProfileContainer>
-  </Wrapper>
-);
+        <h5>My Favourites</h5>
+        <CardsContainer>
+          {user.routine.stretch && (
+            <ActivityCard
+              icon={Stretching}
+              title="Stretching"
+              time={`${user.preferences.stretchTime} mins`}
+            />
+          )}
+          {user.routine.socialMedia && (
+            <ActivityCard icon={Scrolling} title="No Scrolling" />
+          )}
+          <ActivityCard icon={Beverage} title="Beverage" />
+          {user.routine.shower && (
+            <ActivityCard
+              icon={Shower}
+              title="Shower"
+              time={`${user.preferences.showerTime} mins`}
+            />
+          )}
+        </CardsContainer>
+
+        <GoalContainer>
+          <h5>Goal Progress</h5>
+          <GoalCard progress={user.streak} maxDays={user.goal} />
+        </GoalContainer>
+      </ProfileContainer>
+    </Wrapper>
+  ) : (
+    <div>Loading ...</div>
+  );
+};
