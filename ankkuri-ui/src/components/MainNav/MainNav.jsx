19a1a90672909bc49ddb6e7b40b9b7eba1ed219b
Catherine Oxley <catherine.oxley@live.co.uk>
2020-01-07 20:59:38 +0000

    Add mobile navigation (#35)

--- ankkuri-ui/src/components/MainNav/MainNav.jsx
+++ ankkuri-ui/src/components/MainNav/MainNav.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import { MainNavWrapper } from "./MainNav.style";
 import { MenuItem } from "../../elements/atoms";
 
-export default () => (
-  <MainNavWrapper>
-    <ul className="navList">
-      <MenuItem linkName="Home" route="/" />
-      <MenuItem linkName="The Science" route="/the-science" />
-      <MenuItem linkName="Data Privacy" route="/data-privacy" />
-      <MenuItem linkName="Profile" route="/profile" />
-    </ul>
-  </MainNavWrapper>
-);
+export default ({isOpen}) => {
+  console.log(isOpen);
+  return (
+    <MainNavWrapper isOpen={isOpen}>
+      <ul className="navList">
+        <MenuItem linkName="Home" route="/" />
+        <MenuItem linkName="The Science" route="/the-science" />
+        <MenuItem linkName="Data Privacy" route="/data-privacy" />
+        <MenuItem linkName="Profile" route="/profile" />
+      </ul>
+    </MainNavWrapper>
+  );
+};
