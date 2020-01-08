8dd315d684e3fa68042a4c2bddaa88143e116f1e
ashleighyslop <31035314+ashleighyslop@users.noreply.github.com>
2020-01-08 17:30:54 +0000

    styling for mobile view of science and data privacy page (#38)
    * styling for mobile view of science and data privacy page
    
    * additional styling

--- ankkuri-ui/src/elements/molecules/TextPageContainer/TextPageContainer.style.js
+++ ankkuri-ui/src/elements/molecules/TextPageContainer/TextPageContainer.style.js
@@ -30,4 +30,22 @@ export const ContentContainer = styled.div`
       margin: ${({ theme }) => `0 ${rem(theme.spacing.s1)}`};
     }
   }
+
+  @media (max-width: 660px) {
+    width: 80vw;
+    margin-top: 20%;
+    padding-left: 3%;
+    padding-right: 3%;
+    h1{
+      font-size: 30px;
+    }
+    h5{
+      margin-top: 0;
+      margin-bottom: 3%;
+    }
+    p{
+      width: 100%
+      text-align: center
+    }
+  }
 `;
